5661a77b24d14ae179221f47824b38616a3346f7
Tibor Bence Gore <gore.tibor@tell.hu>
2018-04-06 17:01:41 +0200

    Initial Commit

---
+++ TravisTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravisTest
+//
+//  Created by Tibor Bence Gore on 2018. 04. 06..
+//  Copyright © 2018. Tibor Gore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
