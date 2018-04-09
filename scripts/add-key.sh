f80c0a906220f3284924b59c4ae137a2826f131d
Tibor Bence Gore <gore.tibor@tell.hu>
2018-04-09 17:14:55 +0200

    travis 1 cert

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -13,8 +13,8 @@ security unlock-keychain -p $IOS_KEYCHAIN_PWD $IOS_KEYCHAIN_NAME
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/$IOS_KEYCHAIN_NAME
 
 # Add certificates to keychain and allow codesign to access them
-security import ./scripts/certs/dev_public.cer -k ~/Library/Keychains/$IOS_KEYCHAIN_NAME -A
-security import ./scripts/certs/dev_private.p12 -k ~/Library/Keychains/$IOS_KEYCHAIN_NAME -P $IOS_CERT_PASSWORD -A
+#security import ./scripts/certs/dev_public.cer -k ~/Library/Keychains/$IOS_KEYCHAIN_NAME -A
+#security import ./scripts/certs/dev_private.p12 -k ~/Library/Keychains/$IOS_KEYCHAIN_NAME -P $IOS_CERT_PASSWORD -A
 
 security import ./scripts/certs/dist_public.cer -k ~/Library/Keychains/$IOS_KEYCHAIN_NAME -A
 security import ./scripts/certs/dist_private.p12 -k ~/Library/Keychains/$IOS_KEYCHAIN_NAME -P $IOS_CERT_PASSWORD -A
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,19 +3,21 @@
 security list-keychains -s $IOS_KEYCHAIN_NAME
 
 #rm  ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
+#/Users/tiborgore/Library/MobileDevice/Provisioning Profiles
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles/
 
-cp "./scripts/certs/dev.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
+#cp "./scripts/certs/dev.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
 cp "./scripts/certs/adhoc.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
 
 ls
 
 xcrun xcodebuild -workspace TravisTest.xcworkspace -scheme TravisTest -archivePath TravisTest.xcarchive archive
 
-xcrun xcodebuild -exportArchive -archivePath TravisTest.xcarchive -exportPath Ipas/Dev -exportOptionsPlist "./scripts/certs/DevExportOptions.plist"
+#xcrun xcodebuild -exportArchive -archivePath TravisTest.xcarchive -exportPath Ipas/Dev -exportOptionsPlist "./scripts/certs/DevExportOptions.plist"
 
 xcrun xcodebuild -exportArchive -archivePath TravisTest.xcarchive -exportPath Ipas/Adhoc -exportOptionsPlist "./scripts/certs/AdhocExportOptions.plist"
 
-mv Ipas/Dev/TravisTest.ipa ./TravisTest-Dev.ipa
+
+#mv Ipas/Dev/TravisTest.ipa ./TravisTest-Dev.ipa
 mv Ipas/Adhoc/TravisTest.ipa ./TravisTest-Adhoc.ipa
