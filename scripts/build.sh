5617e95b38d49e55d7e4dac7e52621b3fea9f7a0
Tibor Bence Gore <gore.tibor@tell.hu>
2018-04-10 14:10:48 +0200

    travis env reinit

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,8 +16,48 @@ xcodebuild -showBuildSettings
 echo "--------------------------------------------------"
 
 echo "----------No Acrhive-----------"
+cd $SYSTEM_DEVELOPER_UTILITIES_DIR
 ls
-echo "----------Acrhive-----------"
+
+echo "----------Acrhive Settings-----------"
+
+which rvm > /dev/null
+if [[ $? -eq 0 ]]; then
+  echo "RVM detected, forcing to use system ruby"
+  # This allows you to use rvm in a script. Otherwise you get a BS
+  # error along the lines of "cannot use rvm as function". Jeez.
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+  # Cause rvm to use system ruby. AFAIK, this is effective only for
+  # the scope of this script.
+  rvm use system
+fi
+
+if which rbenv > /dev/null; then
+  echo "rbenv detected, removing env variables"
+
+  # Cause rbenv to use system ruby. Lasts only for the scope of this
+  # session which will normally just be this script.
+  rbenv shell system
+
+  unset GEM_HOME
+  unset GEM_PATH
+fi
+
+# Since Xcode has a dependency to 2 external gems: sqlite and CFPropertyList
+# More information https://github.com/fastlane/fastlane/issues/6495
+# We have to unset those variables for rbenv, rvm and when the user uses bundler
+unset RUBYLIB
+unset RUBYOPT
+unset BUNDLE_BIN_PATH
+unset _ORIGINAL_GEM_PATH
+unset BUNDLE_GEMFILE
+
+set -x          # echoes commands
+
+
+
+
 
 xcrun xcodebuild -workspace TravisTest.xcworkspace -scheme TravisTest -archivePath TravisTest.xcarchive archive
 
