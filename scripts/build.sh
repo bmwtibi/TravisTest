a5fe6b37a6df0b10281a738b6a1506c4b948b3d3
Tibor Bence Gore <gore.tibor@tell.hu>
2018-04-10 15:45:07 +0200

    new with build

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,53 +20,56 @@ ls
 
 echo "----------Acrhive Settings-----------"
 
-# which rvm > /dev/null
-# if [[ $? -eq 0 ]]; then
-#   echo "RVM detected, forcing to use system ruby"
-#   # This allows you to use rvm in a script. Otherwise you get a BS
-#   # error along the lines of "cannot use rvm as function". Jeez.
-#   [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-# 
-#   # Cause rvm to use system ruby. AFAIK, this is effective only for
-#   # the scope of this script.
-#   rvm use system
-# fi
-# 
-# if which rbenv > /dev/null; then
-#   echo "rbenv detected, removing env variables"
-# 
-#   # Cause rbenv to use system ruby. Lasts only for the scope of this
-#   # session which will normally just be this script.
-#   rbenv shell system
-# 
-#   unset GEM_HOME
-#   unset GEM_PATH
-# fi
-# 
-# # Since Xcode has a dependency to 2 external gems: sqlite and CFPropertyList
-# # More information https://github.com/fastlane/fastlane/issues/6495
-# # We have to unset those variables for rbenv, rvm and when the user uses bundler
-# unset RUBYLIB
-# unset RUBYOPT
-# unset BUNDLE_BIN_PATH
-# unset _ORIGINAL_GEM_PATH
-# unset BUNDLE_GEMFILE
-# 
-# set -x          # echoes commands
+which rvm > /dev/null
+if [[ $? -eq 0 ]]; then
+  echo "RVM detected, forcing to use system ruby"
+  # This allows you to use rvm in a script. Otherwise you get a BS
+  # error along the lines of "cannot use rvm as function". Jeez.
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+  # Cause rvm to use system ruby. AFAIK, this is effective only for
+  # the scope of this script.
+  rvm use system
+fi
+
+if which rbenv > /dev/null; then
+  echo "rbenv detected, removing env variables"
+
+  # Cause rbenv to use system ruby. Lasts only for the scope of this
+  # session which will normally just be this script.
+  rbenv shell system
+
+  unset GEM_HOME
+  unset GEM_PATH
+fi
+
+# Since Xcode has a dependency to 2 external gems: sqlite and CFPropertyList
+# More information https://github.com/fastlane/fastlane/issues/6495
+# We have to unset those variables for rbenv, rvm and when the user uses bundler
+unset RUBYLIB
+unset RUBYOPT
+unset BUNDLE_BIN_PATH
+unset _ORIGINAL_GEM_PATH
+unset BUNDLE_GEMFILE
+
+set -x          # echoes commands
 
 
 
 
 
-xcrun xcodebuild -workspace TravisTest.xcworkspace -scheme TravisTest -archivePath TravisTest.xcarchive archive
+#xcrun xcodebuild -workspace TravisTest.xcworkspace -scheme TravisTest -archivePath TravisTest.xcarchive archive
+
+xcodebuild archive -workspace TravisTest.xcworkspace -scheme TravisTest -configuration Release -derivedDataPath ./build -archivePath ./build/Products/travistest.xcarchive
 
 ls
 echo "----------Acrhive-----------"
 
 #xcrun xcodebuild -exportArchive -archivePath TravisTest.xcarchive -exportPath Ipas/Dev -exportOptionsPlist "./scripts/certs/DevExportOptions.plist"
 
-xcrun xcodebuild -exportArchive -archivePath TravisTest.xcarchive -exportPath Ipas/Adhoc -exportOptionsPlist "./scripts/certs/AdhocExportOptions.plist"
+#xcrun xcodebuild -exportArchive -archivePath TravisTest.xcarchive -exportPath Ipas/Adhoc -exportOptionsPlist "./scripts/certs/AdhocExportOptions.plist"
 
+xcodebuild -exportArchive -archivePath ./build/Products/travistest.xcarchive -exportOptionsPlist ./scripts/certs/AdhocExportOptions.plist -exportPath ./build/Products/IPA
 
 #mv Ipas/Dev/TravisTest.ipa ./TravisTest-Dev.ipa
-mv Ipas/Adhoc/TravisTest.ipa ./TravisTest-Adhoc.ipa
+mv ./build/Products/IPA ./TravisTest-Adhoc.ipa
